6a375be0056bf2f65a66da6e988c92865ab92a1c
Ramón Blanco Caamaño <ramonblancocaamano@gmail.com>
2019-05-26 21:20:14 +0200

    0.05
    @NEW
    - It is compled the following classes at client side: SwingClient.
    @TODO
    - Debbuging process.
    - General revision.

--- CLIENT/src/main/SwingClient.java
+++ CLIENT/src/main/SwingClient.java
@@ -10,192 +10,323 @@ import javax.swing.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
 import publisher.Publisher;
 import subscriber.Subscriber;
 import topicmanager.TopicManager;
 import topicmanager.TopicManagerStub;
 import webSocketService.WebSocketClient;
 
+/**
+ * @Author: BLANCO CAAMANO, Ramon <ramonblancocaamano@gmail.com>
+ */
 public class SwingClient {
 
-  TopicManager topicManager;
-  public Map<Topic, Subscriber> my_subscriptions;
-  Publisher publisher;
-  Topic publisherTopic;
+    TopicManager topicManager;
+    SubscriberImpl suscriber;
+    public List<Topic> listTopics;
+    public Map<Topic, Subscriber> my_subscriptions;
+    Publisher publisher;
+    Topic publisherTopic;
 
-  JFrame frame;
-  JTextArea topic_list_TextArea;
-  public JTextArea messages_TextArea;
-  public JTextArea my_subscriptions_TextArea;
-  JTextArea publisher_TextArea;
-  JTextField argument_TextField;
+    JFrame frame;
+    public JTextArea topic_list_TextArea;
+    public JTextArea messages_TextArea;
+    public JTextArea my_subscriptions_TextArea;
+    JTextArea publisher_TextArea;
+    JTextField argument_TextField;
+    private ActionEvent e;
 
-  public SwingClient(TopicManager topicManager) {
-    this.topicManager = topicManager;
-    my_subscriptions = new HashMap<Topic, Subscriber>();
-    publisher = null;
-    publisherTopic = null;
-  }
-
-  public void createAndShowGUI() {
-
-    String login = ((TopicManagerStub) topicManager).user.getLogin();
-    frame = new JFrame("Publisher/Subscriber demo, user : " + login);
-    frame.setSize(300, 300);
-    frame.addWindowListener(new CloseWindowHandler());
-
-    topic_list_TextArea = new JTextArea(5, 10);
-    messages_TextArea = new JTextArea(10, 20);
-    my_subscriptions_TextArea = new JTextArea(5, 10);
-    publisher_TextArea = new JTextArea(1, 10);
-    argument_TextField = new JTextField(20);
-
-    JButton show_topics_button = new JButton("show Topics");
-    JButton new_publisher_button = new JButton("new Publisher");
-    JButton new_subscriber_button = new JButton("new Subscriber");
-    JButton to_unsubscribe_button = new JButton("to unsubscribe");
-    JButton to_post_an_event_button = new JButton("post an event");
-    JButton to_close_the_app = new JButton("close app.");
-
-    show_topics_button.addActionListener(new showTopicsHandler());
-    new_publisher_button.addActionListener(new newPublisherHandler());
-    new_subscriber_button.addActionListener(new newSubscriberHandler());
-    to_unsubscribe_button.addActionListener(new UnsubscribeHandler());
-    to_post_an_event_button.addActionListener(new postEventHandler());
-    to_close_the_app.addActionListener(new CloseAppHandler());
-
-    JPanel buttonsPannel = new JPanel(new FlowLayout());
-    buttonsPannel.add(show_topics_button);
-    buttonsPannel.add(new_publisher_button);
-    buttonsPannel.add(new_subscriber_button);
-    buttonsPannel.add(to_unsubscribe_button);
-    buttonsPannel.add(to_post_an_event_button);
-    buttonsPannel.add(to_close_the_app);
-
-    JPanel argumentP = new JPanel(new FlowLayout());
-    argumentP.add(new JLabel("Write content to set a new_publisher / new_subscriber / unsubscribe / post_event:"));
-    argumentP.add(argument_TextField);
-
-    JPanel topicsP = new JPanel();
-    topicsP.setLayout(new BoxLayout(topicsP, BoxLayout.PAGE_AXIS));
-    topicsP.add(new JLabel("Topics:"));
-    topicsP.add(topic_list_TextArea);
-    topicsP.add(new JScrollPane(topic_list_TextArea));
-    topicsP.add(new JLabel("My Subscriptions:"));
-    topicsP.add(my_subscriptions_TextArea);
-    topicsP.add(new JScrollPane(my_subscriptions_TextArea));
-    topicsP.add(new JLabel("I'm Publisher of topic:"));
-    topicsP.add(publisher_TextArea);
-    topicsP.add(new JScrollPane(publisher_TextArea));
-
-    JPanel messagesPanel = new JPanel();
-    messagesPanel.setLayout(new BoxLayout(messagesPanel, BoxLayout.PAGE_AXIS));
-    messagesPanel.add(new JLabel("Messages:"));
-    messagesPanel.add(messages_TextArea);
-    messagesPanel.add(new JScrollPane(messages_TextArea));
-
-    Container mainPanel = frame.getContentPane();
-    mainPanel.add(buttonsPannel, BorderLayout.PAGE_START);
-    mainPanel.add(messagesPanel, BorderLayout.CENTER);
-    mainPanel.add(argumentP, BorderLayout.PAGE_END);
-    mainPanel.add(topicsP, BorderLayout.LINE_START);
-
-    //this is where you restore the user profile:
-    clientSetup();
-
-    frame.pack();
-    frame.setVisible(true);
-  }
-
-  private void clientSetup() {
-    //this is where you restore the user profile:
-    
-    //...
-    
-  }
-
-  class showTopicsHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-    
-    }
-  }
-
-  class newPublisherHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-    
-    }
-  }
-
-  class newSubscriberHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-    
-    }
-  }
-
-  class UnsubscribeHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-    
-    }
-  }
-
-  class postEventHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-    
-    }
-  }
-
-  class CloseAppHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-    
-      System.out.println("one user closed");
-      System.exit(0);
-    }
-  }
-
-  class CloseWindowHandler implements WindowListener {
-
-    public void windowDeactivated(WindowEvent e) {
+    public SwingClient(TopicManager topicManager) {
+        this.topicManager = topicManager;
+        listTopics = new ArrayList<Topic>();
+        my_subscriptions = new HashMap<Topic, Subscriber>();
+        publisher = null;
+        publisherTopic = null;
     }
 
-    public void windowActivated(WindowEvent e) {
+    public void createAndShowGUI() {
+
+        String login = ((TopicManagerStub) topicManager).user.getLogin();
+        frame = new JFrame("Publisher/Subscriber demo, user : " + login);
+        frame.setSize(300, 300);
+        frame.addWindowListener(new CloseWindowHandler());
+
+        topic_list_TextArea = new JTextArea(5, 10);
+        messages_TextArea = new JTextArea(10, 20);
+        my_subscriptions_TextArea = new JTextArea(5, 10);
+        publisher_TextArea = new JTextArea(1, 10);
+        argument_TextField = new JTextField(20);
+
+        JButton show_topics_button = new JButton("show Topics");
+        JButton new_publisher_button = new JButton("new Publisher");
+        JButton new_subscriber_button = new JButton("new Subscriber");
+        JButton to_unsubscribe_button = new JButton("to unsubscribe");
+        JButton to_post_an_event_button = new JButton("post an event");
+        JButton to_close_the_app = new JButton("close app.");
+
+        show_topics_button.addActionListener(new showTopicsHandler());
+        new_publisher_button.addActionListener(new newPublisherHandler());
+        new_subscriber_button.addActionListener(new newSubscriberHandler());
+        to_unsubscribe_button.addActionListener(new UnsubscribeHandler());
+        to_post_an_event_button.addActionListener(new postEventHandler());
+        to_close_the_app.addActionListener(new CloseAppHandler());
+
+        JPanel buttonsPannel = new JPanel(new FlowLayout());
+        buttonsPannel.add(show_topics_button);
+        buttonsPannel.add(new_publisher_button);
+        buttonsPannel.add(new_subscriber_button);
+        buttonsPannel.add(to_unsubscribe_button);
+        buttonsPannel.add(to_post_an_event_button);
+        buttonsPannel.add(to_close_the_app);
+
+        JPanel argumentP = new JPanel(new FlowLayout());
+        argumentP.add(new JLabel("Write content to set a new_publisher / new_subscriber / unsubscribe / post_event:"));
+        argumentP.add(argument_TextField);
+
+        JPanel topicsP = new JPanel();
+        topicsP.setLayout(new BoxLayout(topicsP, BoxLayout.PAGE_AXIS));
+        topicsP.add(new JLabel("Topics:"));
+        topicsP.add(topic_list_TextArea);
+        topicsP.add(new JScrollPane(topic_list_TextArea));
+        topicsP.add(new JLabel("My Subscriptions:"));
+        topicsP.add(my_subscriptions_TextArea);
+        topicsP.add(new JScrollPane(my_subscriptions_TextArea));
+        topicsP.add(new JLabel("I'm Publisher of topic:"));
+        topicsP.add(publisher_TextArea);
+        topicsP.add(new JScrollPane(publisher_TextArea));
+
+        JPanel messagesPanel = new JPanel();
+        messagesPanel.setLayout(new BoxLayout(messagesPanel, BoxLayout.PAGE_AXIS));
+        messagesPanel.add(new JLabel("Messages:"));
+        messagesPanel.add(messages_TextArea);
+        messagesPanel.add(new JScrollPane(messages_TextArea));
+
+        Container mainPanel = frame.getContentPane();
+        mainPanel.add(buttonsPannel, BorderLayout.PAGE_START);
+        mainPanel.add(messagesPanel, BorderLayout.CENTER);
+        mainPanel.add(argumentP, BorderLayout.PAGE_END);
+        mainPanel.add(topicsP, BorderLayout.LINE_START);
+
+        clientSetup();
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
-    public void windowIconified(WindowEvent e) {
+    /*
+     * It restore user's profile. 
+     */
+    private void clientSetup() {
+        showTopicsHandler showTopics = new showTopicsHandler();
+        showPublishersHandler showPublishers = new showPublishersHandler();
+        
+        suscriber = new SubscriberImpl(this);
+        publisher = topicManager.publisherOf();
+        
+        publisherTopic = publisher.topic();
+        
+        showTopics.actionPerformed(e);
+        showPublishers.actionPerformed(e);
+
     }
 
-    public void windowDeiconified(WindowEvent e) {
+    class showTopicsHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            showSubscribersHandler showSubscribers = new showSubscribersHandler();
+
+            listTopics.clear();
+            listTopics.addAll(topicManager.topics());
+
+            topic_list_TextArea.setText("");
+            for (Topic topic : listTopics) {
+                topic_list_TextArea.append(topic.name + "\n");
+            }
+
+            showSubscribers.actionPerformed(e);
+        }
     }
 
-    public void windowClosed(WindowEvent e) {
+    class showSubscribersHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            List<Topic> subscriptions;
+            Subscription_check check;
+            boolean hasTopic;
+
+            subscriptions = new ArrayList<Topic>(my_subscriptions.keySet());
+
+            if (subscriptions.isEmpty()) {
+                my_subscriptions_TextArea.setText("");
+                return;
+            }
+
+            for (int i = 0; i < my_subscriptions.size(); i++) {
+                subscriptions = new ArrayList<Topic>(my_subscriptions.keySet());
+                hasTopic = listTopics.contains(subscriptions.get(i));
+                if (!hasTopic) {
+                    check = topicManager.unsubscribe(subscriptions.get(i), my_subscriptions.get(subscriptions.get(i)));
+                    if (check.result == Subscription_check.Result.NO_SUBSCRIPTION) {
+                        my_subscriptions.remove(subscriptions.get(i), my_subscriptions.get(subscriptions.get(i)));
+                        i = -1;
+                    }
+                }
+            }
+
+            subscriptions = new ArrayList<Topic>(my_subscriptions.keySet());
+            my_subscriptions_TextArea.setText("");
+            for (Topic topic : subscriptions) {
+                my_subscriptions_TextArea.append(topic.name + "\n");
+            }
+        }
     }
 
-    public void windowOpened(WindowEvent e) {
+    class showPublishersHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            publisher_TextArea.setText(publisherTopic.name);
+        }
     }
 
-    public void windowClosing(WindowEvent e) {
-      
-      //...
-    
-      System.out.println("one user closed");
-      System.exit(0);
+    class newPublisherHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            showTopicsHandler showTopics = new showTopicsHandler();
+            showPublishersHandler showPublishers = new showPublishersHandler();
+            String text;
+            Topic topic;
+
+            text = argument_TextField.getText();
+            if (text.equals("")) {
+                showTopics.actionPerformed(e);
+                return;
+            }
+            topic = new Topic(text);
+
+            if (publisherTopic == null) {
+                publisher = topicManager.addPublisherToTopic(topic);
+            } else if (!publisherTopic.name.equals(topic.name)) {
+                topicManager.removePublisherFromTopic(publisherTopic);
+                publisher = topicManager.addPublisherToTopic(topic);
+            }
+            publisherTopic = topic;
+
+            showTopics.actionPerformed(e);
+            showPublishers.actionPerformed(e);
+        }
     }
-  }
+
+    class newSubscriberHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            showTopicsHandler showTopics = new showTopicsHandler();
+            Subscription_check check;
+            String text;
+            Topic topic;
+            boolean hasTopic;
+
+            text = argument_TextField.getText();
+            topic = new Topic(text);
+
+            hasTopic = my_subscriptions.containsKey(topic);
+            if (hasTopic == false) {
+                check = topicManager.subscribe(topic, suscriber);
+                if (check.result == Subscription_check.Result.OKAY) {
+                    my_subscriptions.put(topic, suscriber);
+                }
+            }
+
+            showTopics.actionPerformed(e);
+        }
+    }
+
+    class UnsubscribeHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            showTopicsHandler showTopics = new showTopicsHandler();
+            Subscription_check check;
+            String text;
+            Topic topic;
+            boolean hasTopic;
+
+            text = argument_TextField.getText();
+            topic = new Topic(text);
+
+            hasTopic = my_subscriptions.containsKey(topic);
+            if (hasTopic == true) {
+                check = topicManager.unsubscribe(topic, suscriber);
+                if (check.result == Subscription_check.Result.NO_SUBSCRIPTION) {
+                    my_subscriptions.remove(topic, suscriber);
+                }
+            }
+
+            showTopics.actionPerformed(e);
+        }
+    }
+
+    class postEventHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            showTopicsHandler showTopics = new showTopicsHandler();
+            String text;
+            Message message;
+
+            text = argument_TextField.getText();
+            message = new Message(publisherTopic, text);
+            publisher.publish(message);
+
+            showTopics.actionPerformed(e);
+        }
+    }
+
+    class CloseAppHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("one user closed");
+            System.exit(0);
+        }
+    }
+
+    class CloseWindowHandler implements WindowListener {
+
+        @Override
+        public void windowDeactivated(WindowEvent e) {
+        }
+
+        @Override
+        public void windowActivated(WindowEvent e) {
+        }
+
+        @Override
+        public void windowIconified(WindowEvent e) {
+        }
+
+        @Override
+        public void windowDeiconified(WindowEvent e) {
+        }
+
+        @Override
+        public void windowClosed(WindowEvent e) {
+        }
+
+        @Override
+        public void windowOpened(WindowEvent e) {
+        }
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            System.out.println("one user closed");
+            System.exit(0);
+        }
+    }
 }
--- CLIENT/src/subscriber/SubscriberImpl.java
+++ CLIENT/src/subscriber/SubscriberImpl.java
@@ -1,5 +1,6 @@
 package subscriber;
 
+import java.util.List;
 import util.Subscription_close;
 import entity.Message;
 import entity.Topic;
@@ -13,19 +14,32 @@ import main.SwingClient;
 public class SubscriberImpl implements Subscriber {
 
     private JTextArea messages_TextArea;
+    private JTextArea topic_list_TextArea;
     private JTextArea my_subscriptions_TextArea;
+    private List<Topic> listTopics;
     private Map<Topic, Subscriber> my_subscriptions;
 
     public SubscriberImpl(SwingClient clientSwing) {
         this.messages_TextArea = clientSwing.messages_TextArea;
+        this.topic_list_TextArea = clientSwing.topic_list_TextArea;
         this.my_subscriptions_TextArea = clientSwing.my_subscriptions_TextArea;
+        this.listTopics = clientSwing.listTopics;
         this.my_subscriptions = clientSwing.my_subscriptions;
     }
 
+    @Override
     public void onClose(Subscription_close subs_close) {
         if (subs_close.cause == Subscription_close.Cause.PUBLISHER) {
             messages_TextArea.append("Topic: " + subs_close.topic.name
                     + " has been closed, no publishers left on that topic.\n");
+            if (!listTopics.isEmpty()) {
+                listTopics.remove(subs_close.topic);
+
+                topic_list_TextArea.setText("");
+                for (Topic topic : listTopics) {
+                    topic_list_TextArea.append(topic.name + "\n");
+                }
+            }
         } else if (subs_close.cause == Subscription_close.Cause.SUBSCRIBER) {
             messages_TextArea.append("subscription on topic: " + subs_close.topic.name
                     + " has ended.\n");
@@ -37,6 +51,7 @@ public class SubscriberImpl implements Subscriber {
         }
     }
 
+    @Override
     public void onMessage(Message message) {
         messages_TextArea.append(message.topic.name + ": " + message.content + "\n");
     }
